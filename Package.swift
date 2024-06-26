f66379a641127591fb491b14345c67e8cbff3b8e
Tuan Truong <tuan@Samus.local>
2024-06-26 13:24:17 +0700

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -23,8 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APIService",
-            dependencies: []
+            path: "APIService"
         )
     ],
     swiftLanguageVersions: [.v5]
 )
+
