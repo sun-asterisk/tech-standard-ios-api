5dc5457e37968651835e478c18c74098b0f83b1b
Tuan Truong <tuan@Samus.local>
2024-06-26 13:33:44 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APIService",
-            path: "APIService"
+            path: "APIService/Sources"
         )
     ],
     swiftLanguageVersions: [.v5]
